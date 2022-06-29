574fdc4125fe3dcf6d29119ba45955f73729db8a
mysr97 <104114396+mysr97@users.noreply.github.com>
2022-06-29 16:10:25 +0900

    메뉴 미디어 쿼리 완료

--- js/sr_one.js
+++ js/sr_one.js
@@ -15,14 +15,24 @@ $(window).scroll(function () {
                 $("#menu").css({ "background-color": "rgba(0,0,0,0.7)" });
                 $("#menu > .meLi > li a").css({ "color": "white" });
                 $("#menu > .meLi > li > a > img").css({"filter" : "invert(100%)"});
+                $("#menu > .hambuger > img").attr("src", "img/menu_w.png")
             } else {
                 $("#menu").css({ "background-color": "transparent" });
                 $("#menu > .meLi > li a").css({ "color": "black" });
                 $("#menu > .meLi > li > a > img").css({"filter" : "none"});
+                $("#menu > .hambuger > img").attr("src", "img/menu.png")
             }
         });
         
-        //menu 햄버거 메뉴 적용
+        // 햄버거 메뉴
+        if (window.matchMedia("(max-width: 1000px)").matches) {
+            function openRightMenu() {
+                document.getElementById("right_menu").style.width = "100%";
+            }/*-- #left_menu의 width를 변경시켜 왼쪽 메뉴가 나타날 수 있도록 함.--*/
+            function closeRightMenu() {
+                document.getElementById("right_menu").style.width = "0%";
+            }
+        } 
         
 
 
@@ -203,7 +213,7 @@ $(window).scroll(function () {
 
         //circle
         $(function(){
-            $(".circle2").children("img").delay(4000).animate({opacity:1},700);
+            $(".circle2").children("img").delay(3500).animate({opacity:1},700);
             $(".circle2").children("img").animate({"margin-left":"-40%"},800);
         });
 
